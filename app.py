c895cedc91570d120ac379a777f24e3814897d0d
sevgitr <sevgitr_@hotmail.com>
2023-08-19 01:24:56 +0300

    page routes

--- app.py
+++ app.py
@@ -3,9 +3,26 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
     author_name = "Your Name"  # Değiştirin
     return render_template('index.html', author_name=author_name)
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    return render_template('logout.html')
+
+@app.route('/exam')
+def exam():
+    return render_template('exam.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
+
